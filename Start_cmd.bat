b592f2fffea5d2ef4242ccdd24b88f33f2e9e154
080user080 <080user080@gmail.com>
2026-04-17 00:46:43 +0300

    Ігнорування виконуваних .bat файлів

--- Start_cmd.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d D:\Python\TEXT\LLM_model
-call venv\Scripts\activate.bat
-cd /d D:\Python\TEXT\LLM_model\WhisperModel
-
-cmd /K
-
--- Start_main.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d D:\Python\TEXT\LLM_model
-call venv\Scripts\activate.bat
-cd /d D:\Python\agent
-run_assistant.py
-
-cmd /K
-
